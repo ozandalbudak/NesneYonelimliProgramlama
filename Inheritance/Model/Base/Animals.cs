adf52b273066351e178f9bd1e8816ef9d9afcc4a
Ozan DALBUDAK <odalbudak@isbak.istanbul>
2022-10-17 19:16:39 +0300

    Add project files.

---
+++ Inheritance/Model/Base/Animals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inheritance.Model.Base
+{
+    public class Animals
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+    }
+}
